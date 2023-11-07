de578bc26ea2611a23b7490720bb3aaf16e78886
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-11-07 22:55:22 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+const app = express();
+const PORT = 8888;
+const API_SERVICE_URL = "https://google.com";
+app.use((req, res, next) => {
+  //change anything you want here before proxying the request
+  next();
+});
+app.use(
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+app.listen(PORT, () => {
+  console.log(`Starting Proxy at http://localhost:${PORT}`);
+});
