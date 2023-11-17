02b3de3f0b3264f0e448009281a1fad35cc40386
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-11-17 09:56:05 +0200

    make 2 files

--- libyanspider.ts
+++ libyanspider.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-const PORT = 8888;
-const API_SERVICE_URL = "https://google.com";
+const PORT = process.env.PORT ?? 8888;
+const API_SERVICE_URL = "http://localhost:3002";
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/api", (req, res) => {
+  res.send("Hello World! API");
+});
 app.use((req, res, next) => {
-  //change anything you want here before proxying the request
   next();
 });
 app.use(
